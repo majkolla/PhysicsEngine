067889e6bfc6e5ea96f48d314ac4d3999c63cbd4
michael birtman <micbi949@student.liu.se>
2025-05-06 17:25:07 +0200

    added some changes

--- math.h
+++ math.h
@@ -1,6 +1,10 @@
 #pragma once
-
+#include <cmath> // for sqrt
 // a simple 2D vector struct with the appriorate functions 
+
+// we only need a 2D and a 3D vector so no need to generalize with templates
+
+
 struct Vector2
 {
     float x; 
@@ -13,13 +17,19 @@ struct Vector2
     {
         return Vector2(x+ other.x, y + other.y); 
     }
+    Vector2 operator+=(const Vector2& other) 
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 
     Vector2 operator-(const Vector2& other) const
     {
         return Vector2(x - other.x, y - other.y);
     }
 
-    Vector2 operator*(float scalar) 
+    Vector2 operator*(float scalar) const
     {
         return Vector2(x * scalar, y * scalar);
     }
@@ -49,4 +59,41 @@ struct Vector2
     }
 };
 
-// Implement a 3D vector as well 
+struct Vector3
+{
+    float x; 
+    float y;
+    float z;
+
+    Vector3() : x(0), y(0), z(0) 
+    {}
+    Vector3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) 
+    {}
+
+    Vector3 operator+(const Vector3& other) const
+    {
+        return Vector3(x + other.x, y + other.y, z + other.z);
+    }
+
+    Vector3 operator-(const Vector3& other) const
+    {
+        return Vector3(x - other.x, y - other.y, z - other.z);
+    }
+    Vector3 operator*(float scalar) const
+    {
+        return Vector3(x * scalar, y * scalar, z * scalar);
+    }
+
+    float operator*(const Vector3& other) const
+    {   
+        // Dot product
+        return x*other.x + y*other.y + z*other.z;
+    }
+    Vector3& operator+=(const Vector3& other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+}; 
