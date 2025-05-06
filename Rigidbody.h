a47be7ad94f12d76ab503574b263124f3f1ea7c3
michael birtman <micbi949@student.liu.se>
2025-05-06 17:51:34 +0200

    added some 2D shapes

--- Rigidbody.h
+++ Rigidbody.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Math.h"
+#include "vector"
+// Create a  rigid body class 
+// different shapes that we can add and model physics
 
 struct RigidBody
 {
@@ -28,6 +31,61 @@ struct RigidBody
         position += velocity * dt;
         acceleration = Vector2(0, 0); 
     }
+    void SetPosition(const Vector2& pos)
+    {
+        position = pos;
+    }
+    void SetVelocity(const Vector2& vel)
+    {
+        velocity = vel;
+    }
+    void SetAcceleration(const Vector2& acc)
+    {
+        acceleration = acc;
+    }
+    void SetMass(float m)
+    {
+        mass = m;
+        inverseMass = (mass != 0) ? 1.0f / mass : 0.0f;
+    }
+
 };
 
-// Implement the same thing in 3D later 
+struct Circle : public RigidBody
+{
+    float radius; 
+    Circle(const Vector2& pos, float mass, float radius)
+        : RigidBody(pos, mass), radius(radius) {}
+    // Add more circle specific functions here
+
+
+    void SetRadius(float r)
+    {
+        radius = r;
+    }
+    float GetRadius() const
+    {
+        return radius;
+    }
+    float GetArea() const 
+    {
+        return 3.14f * radius * radius; // pi * r^2
+    }
+}; 
+
+struct Rectangle : public RigidBody
+{
+    float width; 
+    float height; 
+
+    Rectangle(const Vector2& pos, float mass, float width, float height)
+        : RigidBody(pos, mass), width(width), height(height) {}
+    // Add more rectangle specific functions here
+};
+
+// polygon? Maybe we can implement 3D stuff? 
+
+struct Polygon : public RigidBody
+{
+
+}; 
